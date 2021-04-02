5cac17830f382ce2e869acdcc6c5cd616d4d9bdf
Anne <anne.dupin91@gmail.com>
2021-04-02 16:21:40 +0200

    first commit

---
+++ JS/index.js
@@ -0,0 +1,17 @@
+
+function changeImg1(){
+    document.getElementById('img1').src="./img/movie1_2.jpg"
+}
+function changeImg2(){
+    document.getElementById('img2').src="./img/movie2_2.jpg"
+}
+function changeImg3(){
+    document.getElementById('img3').src="./img/movie3_2.jpg"
+}
+function changeImg4(){
+    document.getElementById('img4').src="./img/movie4_2.jpg"
+}
+function changeImg5(){
+    document.getElementById('img5').src="./img/movie5_2.jpg"
+}
+
